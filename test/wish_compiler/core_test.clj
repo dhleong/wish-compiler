6288ab55b72aa4e0f21d6c6b2407738fcf7b32b1
Daniel Leong <me@dhleong.net>
2018-10-08 15:51:59 -0400

    Remove unnecessary core-test

--- test/wish_compiler/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns wish-compiler.core-test
-  (:require [clojure.test :refer :all]
-            [wish-compiler.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
