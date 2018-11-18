688e189a15d59b3cb8d75f858117f7c14746e15e
Daniel Leong <me@dhleong.net>
2018-11-17 19:38:47 -0500

    Update project config

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject wish-compiler "0.1.0-SNAPSHOT"
+(defproject wish-compiler "1.0.0"
   :description "Data Source compiler for wish"
   :url "https://github.com/dhleong/wish-compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [docopt "0.6.1"]
-                 ;; [org.clojure/tools.reader "1.3.0"]
                  [com.cognitect/transit-clj "0.8.313"]]
   :main ^:skip-aot wish-compiler.core
   :target-path "target/%s"
